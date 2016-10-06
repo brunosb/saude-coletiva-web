cabe2aad4a7dc27d60943e72487231629005be45
Bruno Silva Barbosa <barbosa.developer@gmail.com>
2016-10-06 15:33:19 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/saude/coletiva/SaudeColetivaApplication.java
@@ -0,0 +1,12 @@
+package br.com.saude.coletiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaudeColetivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaudeColetivaApplication.class, args);
+	}
+}
