7205db0f4f30362ad28a60bbd5aeaaf75e12a6d2
gabriel <gabriel@laac-01>
2016-10-28 15:24:02 -0300

    coloquei o nav fixo deu trabalho viu huheuhe

--- src/main/java/br/com/saude/coletiva/HomeController.java
+++ src/main/java/br/com/saude/coletiva/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String index(){
 		return "index";
 	}
